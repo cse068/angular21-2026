85a1c2f719857e04421ff607c6de7f9d188711ba
amit nishad <amitsgmic@gmail.com>
2025-11-23 22:39:52 +0530

    # commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('angular-basic-code');
+  protected readonly title = signal('angular-without-ssr');
 }
